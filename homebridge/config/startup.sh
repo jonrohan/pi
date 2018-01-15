c9147c49265853f87fae14ad2050f2913032cb64
Jon Rohan <rohan@github.com>
2018-01-14 22:33:30 -0800

    First commit

---
+++ homebridge/config/startup.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+#
+# Docker Homebridge Custom Startup Script - oznu/homebridge
+#
+# This script can be used to customise the environment and will be executed as
+# the root user each time the container starts.
+#
+# If using this script to install Homebridge plugins please take note:
+#   * DO NOT install plugins using the global flag (-g)
+#   * ALWAYS install plugins using the npm --save flag, or just use yarn add
+#
+# Example:
+#
+# npm install --save homebridge-hue
+#
+# Consider using yarn to install plugins instead (it's much faster):
+#
+# yarn add homebridge-hue
+#
+
+# Replace environment variables
+cat config.json | perl -pe 's/\$(\{)?([a-zA-Z_]\w*)(?(1)\})/$ENV{$2}/g' | tee config.json
